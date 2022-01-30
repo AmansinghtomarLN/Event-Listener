fa2f839209b6ecad5015085cedc7ce9e1c4fdffa
Aman Singh Tomar <amansinghtomar2209@gmail.com>
2022-01-31 02:54:26 +0530

    Spring Build in Listener added : ContextRefreshedListener

--- src/main/java/com/spring/config/Start.java
+++ src/main/java/com/spring/config/Start.java
@@ -2,20 +2,21 @@ package com.spring.config;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.event.ContextRefreshedEvent;
 
 import com.spring.event.BigBangTheory;
+import com.spring.listener.ContextRefreshedListener;
 import com.spring.publisher.Netflix;
 
 public class Start {
 
-	
 	public static void main(String[] args) {
 		ApplicationContext context = new AnnotationConfigApplicationContext(AppConfiguration.class);
 		Netflix netflix = context.getBean("netflix", Netflix.class); // First character must be small in
-																					// component name
+																		// component name
+	//	ContextRefreshedListener refresh = context.getBean("contextRefreshedListener",ContextRefreshedListener.class);
 		netflix.stream_BigBangTheory();
 		netflix.stream_ComedyNights();
 	}
-
 
 }
--- src/main/java/com/spring/listener/Amisha.java
+++ src/main/java/com/spring/listener/Amisha.java
@@ -1,4 +1,4 @@
-package com.spring.subscriber;
+package com.spring.listener;
 
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
